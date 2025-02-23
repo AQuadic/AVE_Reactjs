80d7b1d6535ae2d96703a59389d2a116a8e669d3
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-02-23 14:58:13 +0200

    Add background in mobile view

--- src/components/HomePage/ProcessStep.jsx
+++ src/components/HomePage/ProcessStep.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
 const ProcessStep = ({ iconSrc, number, title }) => (
-    <div className="flex flex-col items-center lg:w-64  md:w-56 w-32">
+    <div className="flex flex-col mx-auto items-center lg:w-64  md:w-56 w-32">
         <div className="flex items-baseline gap-2">
             <img src={iconSrc} className='md:w-12 md:h-12 w-6 h-6' alt="Icon" />
             <p className="text-[#1E1E1E] md:text-[32px] text-xl font-bold">{number}</p>
