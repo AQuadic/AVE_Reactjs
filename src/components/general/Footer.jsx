55ddfe9d41eec656721bcef86b07e5a0531c4bd7
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-02-24 14:06:51 +0200

    Footer translation

--- src/components/general/Footer.jsx
+++ src/components/general/Footer.jsx
@@ -7,17 +7,19 @@ import whatsapp from '../../assets/images/whatsapp.svg'
 import x from '../../assets/images/x.svg'
 import check from '../../assets/images/check.svg'
 import { Link } from 'react-router'
+import { useTranslation } from "react-i18next";
 const Footer = () => {
+    const { t } = useTranslation("navbar");
     return (
         <div className='relative'>
-            <footer className="bg-[#1E1E1E] lg:h-[372px]">
-                <div className="mx-auto w-full max-w-screen-2xl p-4 py-6 lg:py-8">
-                    <div className="lg:flex xl:gap-32 lg:gap-20">
+            <footer className="bg-[#1E1E1E] xl:h-[372px]">
+                <div className="container w-full max-w-screen-2xl p-4 py-6 lg:py-8">
+                    <div className="xl:flex xl:gap-32 lg:gap-20">
                         <div className="mb-6 md:mb-0">
                             <Link to='/' className="flex items-center justify-center md:justify-start">
                                 <img src={Logo} alt="Logo" />
                             </Link>
-                            <p className='md:w-[373px] text-base leading-6 text-white md:text-start text-center'>العربية لمعدات المركبات (AVE) هي إحدى الشركات الرائدة في مجال معدات السيارات. متخصصون في توريد سيارات الرعاية الصحية (سيارات الإسعاف ،العناية المركزة)</p>
+                            <p className='md:w-[373px] text-base leading-6 text-white md:text-start text-center'>{t('paragraph')}</p>
                             <div className='flex items-center md:justify-start justify-center gap-3 mt-[18px]'>
                                 <Link to=''><img src={LinkedIn} className='w-8 h-8' alt="LinkedIn" /></Link>
                                 <Link to=''><img src={youtube} className='w-8 h-8' alt="youtube" /></Link>
@@ -27,78 +29,78 @@ const Footer = () => {
                                 <Link to=''><img src={x} className='w-8 h-8' alt="x" /></Link>
                             </div>
                         </div>
-                        <div className="grid grid-cols-1 md:gap-10 xl:gap-24 sm:grid-cols-3 mt-5 lg:mt-0">
+                        <div className="flex md:flex-row flex-col lg:gap-20 md:gap-5 mt-5 xl:mt-0">
                             <div className='text-center'>
-                                <h2 className="text-2xl font-semibold text-white">الخدمات</h2>
+                                <h2 className="text-2xl font-semibold text-white">{t('services')}</h2>
                                 <div className='w-6 h-1 bg-[#ED1C24] rounded-sm mt-4 mb-2 mx-auto'></div>
-                                <ul className="text-white font-normal text-base flex md:flex-row flex-col items-center justify-center">
+                                <ul className="text-white font-normal text-base flex md:flex-row flex-col items-center justify-center gap-3">
                                     <div className='order-2 md:order-2 flex flex-col items-center md:items-start'>
                                         <li className="mb-4 flex items-center gap-[9px]">
                                             <img src={check} alt="Check" className='md:block hidden' />
-                                            <Link to='/' >لذوي الهمم</Link>
+                                            <Link to='/' >{t('disabilities')}</Link>
                                         </li>
                                         <li className="mb-4 flex items-center gap-[9px]">
                                             <img src={check} alt="Check" className='md:block hidden' />
-                                            <Link to='/' >عيادات متنقلة</Link>
+                                            <Link to='/' >{t('clinics')}</Link>
                                         </li>
                                         <li className="mb-4 flex items-center gap-[9px]">
                                             <img src={check} alt="Check" className='md:block hidden' />
-                                            <Link to='/' >ورش متنقلة</Link>
+                                            <Link to='/' >{t('workshop')}</Link>
                                         </li>
                                         <li className="mb-4 flex items-center gap-[9px]">
                                             <img src={check} alt="Check" className='md:block hidden' />
-                                            <Link to='/' >مقهى</Link>
+                                            <Link to='/' >{t('cafe')}</Link>
                                         </li>
                                     </div>
                                     <div className='order-1 md:order-1 flex flex-col items-center md:items-start'>
                                         <li className="mb-4 flex items-center gap-[9px]">
                                             <img src={check} alt="Check" className='md:block hidden' />
-                                            <Link to='/' >المختبر المتنقل</Link>
+                                            <Link to='/' >{t('lab')}</Link>
                                         </li>
                                         <li className="mb-4 flex items-center gap-[9px]">
                                             <img src={check} alt="Check" className='md:block hidden' />
-                                            <Link to='/ambEquipmenet' >تجهيز  إسعاف</Link>
+                                            <Link to='/ambEquipmenet' >{t('ambEquip')}</Link>
                                         </li>
                                         <li className="mb-4 flex items-center gap-[9px]">
                                             <img src={check} alt="Check" className='md:block hidden' />
-                                            <Link to='/vip' >كبار الشخصيات</Link>
+                                            <Link to='/vip' >{t('vip')}</Link>
                                         </li>
                                         <li className="mb-4 flex items-center gap-[9px]">
                                             <img src={check} alt="Check" className='md:block hidden' />
-                                            <Link to='/' >ورشة متنقلة</Link>
+                                            <Link to='/' >{t('workshop')}</Link>
                                         </li>
                                     </div>
                                 </ul>
                             </div>
                             <div className='flex flex-col items-center md:items-start'>
-                                <h2 className="text-2xl font-semibold text-white">الشركة</h2>
+                                <h2 className="text-2xl font-semibold text-white">{t('company')}</h2>
                                 <div className='w-6 h-1 bg-[#ED1C24] rounded-sm mt-4 mb-2'></div>
                                 <ul className="text-white font-normal text-base flex flex-col md:items-start items-center">
                                     <li className="mb-4 flex items-center gap-[9px]">
                                         <img src={check} alt="Check" className='md:block hidden' />
-                                        <Link to='/whoWeAre' >من نحن</Link>
+                                        <Link to='/whoWeAre' >{t('whoWeAre')}</Link>
                                     </li>
                                     <li className="mb-4 flex items-center gap-[9px]">
                                         <img src={check} alt="Check" className='md:block hidden' />
-                                        <Link to='/contactUs' >تواصل معنا</Link>
+                                        <Link to='/contactUs' >{t('contactUs')}</Link>
                                     </li>
                                     <li className="mb-4 flex items-center gap-[9px]">
                                         <img src={check} alt="Check" className='md:block hidden' />
-                                        <Link to='/faqs' >الاسئلة الشائعة</Link>
+                                        <Link to='/faqs' >{t('faq')}</Link>
                                     </li>
                                 </ul>
                             </div>
                             <div className='flex flex-col items-center md:items-start'>
-                                <h2 className="text-2xl font-semibold text-white">قانونى</h2>
+                                <h2 className="text-2xl font-semibold text-white">{t('legal')}</h2>
                                 <div className='w-6 h-1 bg-[#ED1C24] rounded-sm mt-4 mb-2'></div>
                                 <ul className="text-white font-normal text-base flex flex-col md:items-start items-center">
                                     <li className="mb-4 flex items-center gap-[9px]">
                                         <img src={check} alt="Check" className='md:block hidden' />
-                                        <Link to='/terms' >الشروط والاحكام</Link>
+                                        <Link to='/terms' >{t('terms')}</Link>
                                     </li>
                                     <li className="mb-4 flex items-center gap-[9px]">
                                         <img src={check} alt="Check" className='md:block hidden' />
-                                        <Link to='/privacy' >سياسة الخصوصية</Link>
+                                        <Link to='/privacy' >{t('privacy')}</Link>
                                     </li>
                                 </ul>
                             </div>
