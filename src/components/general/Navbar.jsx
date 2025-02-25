3c09eb9aa52abd5c05a0e9c05287e8120628c018
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-02-25 11:39:45 +0200

    Add language in sidebar

--- src/components/general/Navbar.jsx
+++ src/components/general/Navbar.jsx
@@ -1,20 +1,18 @@
 import { NavLink } from 'react-router'
 import Logo from '../../assets/images/navbarLogo.svg'
-import global from '../../assets/images/global.svg'
 import arrow from '../../assets/images/arrow_down.svg'
 import toggle from '../../assets/images/toggle.svg'
 import sidebarMob from '../../assets/images/sidebarMob.webp'
 import closeIcon from '../../assets/images/closeIcon.svg'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useTranslation } from "react-i18next";
 import "../../Hooks/i18n";
+import Language from './Language'
 
 const Navbar = () => {
-    const { i18n } = useTranslation();
     const { t } = useTranslation("navbar");
 
     const [isOpen, setIsOpen] = useState(false);
-    const [isOpenLang, setIsOpenLang] = useState(false);
     const toggleDropdown = () => {
         setIsOpen(!isOpen);
     };
@@ -22,28 +20,6 @@ const Navbar = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
     const toggleSidebar = () => {
         setIsSidebarOpen(!isSidebarOpen);
-    };
-    const savedLanguage = localStorage.getItem("language") || i18n.language;
-    const [language, setLanguage] = useState(savedLanguage);
-
-    const languages = [
-        { code: "en", name: "English", flag: "https://purecatamphetamine.github.io/country-flag-icons/3x2/US.svg" },
-        { code: "ar", name: "العربية", flag: "https://purecatamphetamine.github.io/country-flag-icons/3x2/AE.svg" },
-    ];
-
-    useEffect(() => {
-        i18n.changeLanguage(language);
-        document.documentElement.dir = language === "ar" ? "rtl" : "ltr";
-        document.body.dir = language === "ar" ? "rtl" : "ltr";
-    }, [language, i18n]);
-
-    const handleLanguageChange = (langCode) => {
-        localStorage.setItem("language", langCode);
-        setLanguage(langCode);
-        i18n.changeLanguage(langCode);
-        document.documentElement.dir = langCode === "ar" ? "rtl" : "ltr";
-        document.body.dir = langCode === "ar" ? "rtl" : "ltr";
-        setIsOpenLang(false);
     };
 
     return (
@@ -100,10 +76,7 @@ const Navbar = () => {
                             <NavLink to='/whoWeAre' className="block py-1 px-4 md:p-0 text-xs text-[#1E1E1E] transform hover:text-[#D82022] hover:underline transition-all duration-300">{t('whoWeAre')}</NavLink>
                             <NavLink to='/contactUs' className="block py-1 px-4 md:p-0 text-xs text-[#1E1E1E] transform hover:text-[#D82022] hover:underline transition-all duration-300">{t('contactUs')}</NavLink>
                             <NavLink to='/faqs' className="block py-1 px-4 md:p-0 text-xs text-[#1E1E1E] transform hover:text-[#D82022] hover:underline transition-all duration-300">{t('faq')}</NavLink>
-                            <button type="button" data-dropdown-toggle="language-dropdown-menu" className="md:w-[125px] w-[100px] md:h-14 inline-flex gap-2 items-center justify-center px-4 py-2 text-xs text-[#1E1E1E] rounded-lg cursor-pointer">
-                                <img src={global} alt="Language" />
-                                <p>العربية</p>
-                            </button>
+                            <Language />
                             <div className='h-px w-60 bg-[#EAEAEA] mx-auto'></div>
                             <div className='px-4 flex gap-2'>
                                 <img src={sidebarMob} alt="Phone" />
@@ -115,30 +88,8 @@ const Navbar = () => {
                         </div>
                     </div>
                     <div className="flex items-center md:order-2 space-x-1 md:space-x-0 rtl:space-x-reverse">
-                        <div className="relative flex items-center">
-                            <button
-                                type="button"
-                                className="md:w-[125px] w-[100px] md:h-14 border md:inline-flex hidden gap-2 items-center justify-center px-4 py-2 text-lg font-bold text-[#1E1E1E] rounded-lg cursor-pointer"
-                                onClick={() => setIsOpenLang(!isOpenLang)}
-                            >
-                                <img src={global} alt="Language" />
-                                <p style={{ color: language === "ar" }}>{language === "ar" ? "English" : "العربية"}</p>
-                            </button>
-
-                            {isOpenLang && (
-                                <div className="absolute top-12 rtl:left-0 ltr:right-0 bg-white border border-gray-200 rounded-lg shadow-lg z-10 w-32">
-                                    {languages.map((lang) => (
-                                        <div
-                                            key={lang.code}
-                                            className="px-4 py-2 hover:bg-gray-100 cursor-pointer flex items-center gap-2"
-                                            onClick={() => handleLanguageChange(lang.code)}
-                                        >
-                                            <img className="w-5" src={lang.flag} alt={lang.name} />
-                                            <span>{lang.name}</span>
-                                        </div>
-                                    ))}
-                                </div>
-                            )}
+                        <div className='md:inline-flex hidden'>
+                            <Language />
                         </div>
                         <div className='w-20 h-6 bg-[#D820221A] rounded-sm md:hidden flex items-center justify-center'>
                             <p className='text-[#D82022] font-normal text-xs'>{t('downloadApp')}</p>
