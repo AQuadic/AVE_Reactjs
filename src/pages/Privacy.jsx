609d5cdf2bf96a8e5f6820cbeef6c77ba89f3758
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-05-11 18:32:12 +0300

    Privacy page bg image

--- src/pages/Privacy.jsx
+++ src/pages/Privacy.jsx
@@ -1,15 +1,17 @@
-import Header from '../components/general/Header'
+// import Header from '../components/general/Header'
 import Content from '../components/Privacy/Content'
-import header from '../assets/images/header.webp'
+// import header from '../assets/images/header.webp'
+import MainImage from '@/components/general/MainImage'
 
 const Privacy = () => {
     return (
         <dev>
-            <Header
+            {/* <Header
                 title="العربية لمعدات المركبات (AVE)"
                 description="العربية لمعدات المركبات (AVE) هي إحدى الشركات الرائدة في مجال معدات السيارات. متخصصون في توريد سيارات الرعاية الصحية (سيارات الإسعاف ،العناية المركزة)"
                 imageUrl={header}
-            />
+            /> */}
+            <MainImage />
             <Content />
         </dev>
     )
