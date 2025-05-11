8791065bda3c274bdb6b626fb09f2b51d780f6bc
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-05-11 21:08:44 +0300

    fix pages main image

--- src/components/general/MainImage.jsx
+++ src/components/general/MainImage.jsx
@@ -1,8 +1,10 @@
+import mainImage from '@/assets/images/home/MainImage.png'
+import redLine from '@/assets/images/home/redLine.png'
 const MainImage = () => {
     return (
         <div className="relative z-10 mt-5">
-            <img src="/dist/assets/MainImage.png" className="w-full" alt="Main Image" />
-            <img src="/dist/assets/redLine.png" className="w-full absolute bottom-[6%] left-0 -z-10" alt="Red Line" />
+            <img src={mainImage} className="w-full" alt="Main Image" />
+            <img src={redLine} className="w-full absolute bottom-[6%] left-0 -z-10" alt="Red Line" />
         </div>
     )
 }
