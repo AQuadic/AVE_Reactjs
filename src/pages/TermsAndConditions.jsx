fb844b17650fccc693d87de2be1d54c96df7cc1d
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-05-11 18:32:59 +0300

    Terms And Conditions bg image

--- src/pages/TermsAndConditions.jsx
+++ src/pages/TermsAndConditions.jsx
@@ -1,15 +1,17 @@
-import Header from "../components/general/Header"
+// import Header from "../components/general/Header"
+import MainImage from "@/components/general/MainImage"
 import Content from "../components/TermsAndConditions/Content"
-import header from '../assets/images/header.webp'
+// import header from '../assets/images/header.webp'
 
 const TermsAndConditions = () => {
     return (
         <div>
-            <Header
+            {/* <Header
                 title="العربية لمعدات المركبات (AVE)"
                 description="العربية لمعدات المركبات (AVE) هي إحدى الشركات الرائدة في مجال معدات السيارات. متخصصون في توريد سيارات الرعاية الصحية (سيارات الإسعاف ،العناية المركزة)"
                 imageUrl={header}
-            />
+            /> */}
+            <MainImage />
             <Content />
         </div>
     )
