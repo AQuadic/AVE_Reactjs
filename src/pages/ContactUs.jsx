81132e656cdd64b9a9ce11d6d15eaba7a92b466a
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-05-11 18:33:58 +0300

    contact us bg image

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,16 +1,18 @@
 import Form from "../components/ContactUs/Form"
-import Header from "../components/general/Header"
-import header from '../assets/images/header.webp'
-import { useTranslation } from "react-i18next";
+// import Header from "../components/general/Header"
+// import header from '../assets/images/header.webp'
+// import { useTranslation } from "react-i18next";
+import MainImage from "@/components/general/MainImage";
 const ContactUs = () => {
-    const { t } = useTranslation("contactUs");
+    // const { t } = useTranslation("contactUs");
     return (
         <div>
-            <Header
+            {/* <Header
                 title={t('headTitle')}
                 description={t('paraTitle')}
                 imageUrl={header}
-            />
+            /> */}
+            <MainImage />
             <Form />
         </div>
     )
