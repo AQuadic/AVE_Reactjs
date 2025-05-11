b4a42dad80e90029fb1b3440842d63e4328bd499
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-05-11 17:04:45 +0300

    vip page title

--- src/components/Services/VIP/Content.jsx
+++ src/components/Services/VIP/Content.jsx
@@ -6,6 +6,7 @@ const Content = () => {
         <section>
             <img src="/dist/assets/VIP/newVIP.png" className='w-full md:mt-24' alt="Image" />
             <div className='container mx-auto flex flex-wrap items-center justify-between md:my-[88px] mt-5'>
+                <h1 className="text-[#1E1E1E] text-[32px] font-bold md:mb-16 mb-4">{t("title")}</h1>
                 <div className="flex lg:flex-row flex-col lg:gap-0 gap-4 items-center justify-between w-full">
                     <div className="text-[#505050] text-base font-normal flex flex-col gap-2 lg:w-[719px]">
                         <p>{t('fPara')}</p>
