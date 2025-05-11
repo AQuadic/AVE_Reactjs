046a46d77c34cde5288bfbc4cb93adac02ebc71c
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-05-11 18:35:28 +0300

    "who we are section" bg image

--- src/pages/WhoWeAre.jsx
+++ src/pages/WhoWeAre.jsx
@@ -1,19 +1,21 @@
-import Header from '../components/general/Header'
+// import Header from '../components/general/Header'
+import MainImage from '@/components/general/MainImage'
 import AboutUs from '../components/WhoWeAre/AboutUs'
 import OurPrinciples from '../components/WhoWeAre/OurPrinciples'
 import OurCertificates from '../components/general/OurCertificates'
-import header from '../assets/images/header.webp'
-import { useTranslation } from "react-i18next";
+// import header from '../assets/images/header.webp'
+// import { useTranslation } from "react-i18next";
 
 const WhoWeAre = () => {
-    const { t } = useTranslation("whoWeAre");
+    // const { t } = useTranslation("whoWeAre");
     return (
         <div>
-            <Header
+            {/* <Header
                 title={t('headTitle')}
                 description={t('paraTitle')}
                 imageUrl={header}
-            />
+            /> */}
+            <MainImage />
             <AboutUs />
             <OurPrinciples />
             <OurCertificates />
