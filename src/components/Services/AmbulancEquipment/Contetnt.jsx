95c200c3188b6940a785c54e1c286a57bc2e906e
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-05-11 20:45:20 +0300

    fix ambulance equipment images

--- src/components/Services/AmbulancEquipment/Contetnt.jsx
+++ src/components/Services/AmbulancEquipment/Contetnt.jsx
@@ -1,10 +1,16 @@
 // import check from '../../../assets/images/red_check.svg'
 import { useTranslation } from "react-i18next";
+import ambulance from '@/assets/images/Ambulance/ambulance.png'
+import ambulance1 from '@/assets/images/Ambulance/ambulance1.png'
+import ambulance2 from '@/assets/images/Ambulance/ambulance2.png'
+import ambulance3 from '@/assets/images/Ambulance/ambulance3.png'
+
+
 const Contetnt = () => {
     const { t } = useTranslation("ambequipment");
     return (
         <section>
-            <img src="../../../../dist/assets/Ambulance/ambulance.png" className='w-full md:mt-24' alt="Ambulance" />
+            <img src={ambulance} className='w-full md:mt-24' alt="Ambulance" />
             <div className='container mx-auto flex flex-wrap items-center justify-between md:my-[88px] mt-5'>
                 <h1 className="text-[#1E1E1E] text-[32px] font-bold md:mb-16 mb-4">{t("title")}</h1>
                 <div className="flex lg:flex-row flex-col lg:gap-0 gap-4 items-center justify-between w-full">
@@ -19,11 +25,11 @@ const Contetnt = () => {
                         <p>{t('sevPara')}</p>
                         <p>{t('eiPara')}</p>
                     </div>
-                    <img src="/dist/assets/Ambulance/ambulance1.png" alt="Ambulance" />
+                    <img src={ambulance1} alt="Ambulance" />
                 </div>
 
                 <div className="flex lg:flex-row flex-col lg:gap-0 gap-4 items-center justify-between w-full mt-16">
-                    <img src="/dist/assets/Ambulance/ambulance2.png" alt="Ambulance" />
+                    <img src={ambulance2} alt="Ambulance" />
                     <div className="text-[#505050] text-base font-normal flex flex-col gap-2 lg:w-[719px]">
                         <p>{t('fPara')}</p>
                         <p>{t('sPara')}</p>
@@ -47,7 +53,7 @@ const Contetnt = () => {
                         <p>{t('sevPara')}</p>
                         <p>{t('eiPara')}</p>
                     </div>
-                    <img src="/dist/assets/Ambulance/ambulance3.png" alt="Ambulance" />
+                    <img src={ambulance3} alt="Ambulance" />
                 </div>
 
             </div>
