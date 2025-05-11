c50cc65c6547292ebe4c96b05cf857c73e751248
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-05-11 21:05:02 +0300

    fix ice cream images

--- src/components/Services/IceCream/Content.jsx
+++ src/components/Services/IceCream/Content.jsx
@@ -1,9 +1,14 @@
 import { useTranslation } from "react-i18next";
+import IceCream from '@/assets/images/Icecream/IceCream.png'
+import IceCream1 from '@/assets/images/Icecream/IceCream1.png'
+import IceCream2 from '@/assets/images/Icecream/IceCream2.png'
+import IceCream3 from '@/assets/images/Icecream/IceCream3.png'
+
 const Content = () => {
     const { t } = useTranslation("icecream");
     return (
         <section>
-            <img src="/dist/assets/Icecream/IceCream.png" className='w-full md:mt-24' alt="Image" />
+            <img src={IceCream} className='w-full md:mt-24' alt="Image" />
             <div className='container mx-auto flex flex-wrap items-center justify-between md:my-[88px] mt-5'>
                 <h1 className="text-[#1E1E1E] text-[32px] font-bold md:mb-16 mb-4">{t("title")}</h1>
                 <div className="flex lg:flex-row flex-col lg:gap-0 gap-4 items-center justify-between w-full">
@@ -18,11 +23,11 @@ const Content = () => {
                         <p>{t('sevPara')}</p>
                         <p>{t('eiPara')}</p>
                     </div>
-                    <img src="/dist/assets/Icecream/icecream1.png" alt="Ice Cream" />
+                    <img src={IceCream1} alt="Ice Cream" />
                 </div>
 
                 <div className="flex lg:flex-row flex-col lg:gap-0 gap-4 items-center justify-between w-full mt-16">
-                    <img src="/dist/assets/Icecream/icecream2.png" className="" alt="Ice Cream" />
+                    <img src={IceCream2} className="" alt="Ice Cream" />
                     <div className="text-[#505050] text-base font-normal flex flex-col gap-2 lg:w-[719px]">
                         <p>{t('fPara')}</p>
                         <p>{t('sPara')}</p>
@@ -46,7 +51,7 @@ const Content = () => {
                         <p>{t('sevPara')}</p>
                         <p>{t('eiPara')}</p>
                     </div>
-                    <img src="/dist/assets/Icecream/icecream3.png" alt="Ice Cream" />
+                    <img src={IceCream3} alt="Ice Cream" />
                 </div>
 
             </div>
