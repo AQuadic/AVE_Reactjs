cdb6710e68791b3b54a43fac943f2afdc12c5a34
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-05-12 16:49:39 +0300

    home page translation

--- src/components/HomePage/AboutUs.jsx
+++ src/components/HomePage/AboutUs.jsx
@@ -1,5 +1,14 @@
 import AboutUsImg from '@/assets/images/home/aboutUsimg.png'
+import { useState } from 'react';
+import { useTranslation } from "react-i18next";
+
 const AboutUs = () => {
+    const { t } = useTranslation("home");
+    const [showMore, setShowMore] = useState(false);
+
+    const toggleShowMore = () => {
+        setShowMore(!showMore);
+    };
     return (
         <section className="container flex xl:flex-row flex-col items-center justify-between md:mt-20">
             <img className='lg:w-[563px] lg:h-[449px] w-[228.6px] h-[182px]' src={AboutUsImg} alt="Image" />
@@ -7,13 +16,25 @@ const AboutUs = () => {
                 <div className='flex items-center gap-4'>
                     <div className="w-2 md:h-[46px] h-7 bg-[#D82022] rounded-sm"></div>
                     <h1 className="font-bold md:text-[40px] text-xl text-[#1E1E1E]">
-                        نبذه عنا
+                        {t('aboutus')}
                     </h1>
                 </div>
-                <p className='text-[#1E1E1E] lg:text-[32px] text-xl font-bold mt-4'>&quot;العربية لمعدات المركبات
+                <p className='text-[#1E1E1E] lg:text-[32px] text-xl font-bold mt-4'>&quot; {t('aboutTitle')}
                     ( <span className='text-[#D82022]'>AVE</span>)&quot;
                 </p>
-                <p className='text-[#505050] lg:text-2xl text-base lg:font-bold font-normal md:w-[570px] mt-4'>هي إحدى الشركات العربية الرائدة في مجال معدات السيارات. متخصصون في توريد سيارات الرعاية الصحية (سيارات الإسعاف ،العناية المركزة) ليس في مصر فقط. وكذلك في الشرق الأوسط في مجال دقيق وصعب. وعندما برعت الشركة علي مدار 15 عاماً في هذا المجال مع توفير أفضل انواع التكنولوجيا الحديثة في تجهيز السيارات... <span className='text-[#D82022]'>أقرا المذيد</span></p>
+                <p className='text-[#505050] lg:text-2xl text-base lg:font-bold font-normal md:w-[570px] mt-4'>
+                    {t('aboutDescription')}
+                    {showMore && (
+                        <span> {t('aboutDescription')}</span>
+                    )}
+                    <span
+                        className='text-[#D82022] cursor-pointer'
+                        onClick={toggleShowMore}
+                    >
+                        {' '}
+                        {showMore ? t('readLess') : t('readMore')}
+                    </span>
+                </p>
             </div>
         </section>
     )
