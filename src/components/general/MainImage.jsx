2c578d31edf951027cd90a42355779f088ebc3be
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-05-12 17:42:52 +0300

    change image width

--- src/components/general/MainImage.jsx
+++ src/components/general/MainImage.jsx
@@ -3,7 +3,7 @@ import redLine from '@/assets/images/home/redLine.png'
 const MainImage = () => {
     return (
         <div className="relative z-0 mt-5">
-            <img src={mainImage} className="w-full" alt="Main Image" />
+            <img src={mainImage} className="lg:w-[928px] w-full lg:h-[560px] mx-auto" alt="Main Image" />
             <img src={redLine} className="w-full absolute bottom-[6%] left-0 -z-10" alt="Red Line" />
         </div>
     )
