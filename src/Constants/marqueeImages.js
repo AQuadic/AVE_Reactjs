beabfd260c03e39e8a847c17c4a349b198ceb975
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-05-12 18:12:37 +0300

    our clients

---
+++ src/Constants/marqueeImages.js
@@ -0,0 +1,9 @@
+export const marqueeImages = [
+  "/images/techLogos/android.svg",
+  "/images/techLogos/angular.svg",
+  "/images/techLogos/figma.svg",
+  "/images/techLogos/flutter.svg",
+  "/images/techLogos/ios.svg",
+  "/images/techLogos/javascript.svg",
+  "/images/techLogos/laravel.svg",
+];
