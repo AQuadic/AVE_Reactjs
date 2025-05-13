729b906750de51f7181b071ad1f1da9495eac5ee
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-05-13 10:26:54 +0300

    link products to health car page

--- src/components/HealthCare/Description.jsx
+++ src/components/HealthCare/Description.jsx
@@ -6,7 +6,7 @@ const Description = () => {
     const { t } = useTranslation("home");
     return (
         <section className="container">
-            <div className="lg:w-[1200px] w-full h-auto border border-[#CBCBCB] rounded-3xl mb-8 pb-8">
+            <div className="xl:w-[1200px] w-full h-auto border border-[#CBCBCB] rounded-3xl mb-8 pb-8">
                 <div className='flex items-center justify-around mt-16'>
                     <div className='flex flex-col items-center'>
                         <img src={ICU} className='md:w-[200px] w-[120px] md:h-[200px] h-[120px] rounded-full' alt="Image" />
