6c6ae517ee05d7eed9a2f52773a0a1edb0f252b8
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-05-14 19:06:12 +0300

    Location

--- src/components/ContactUs/Form.jsx
+++ src/components/ContactUs/Form.jsx
@@ -48,7 +48,7 @@ const Form = () => {
             </div>
             <div className="google-map-code">
                 <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15282225.79979123!2d73.7250245393691!3d20.750301298393563!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff06b92b791%3A0xd78c4fa1854213a6!2sIndia!5e0!3m2!1sen!2sin!4v1587818542745!5m2!1sen!2sin"
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d27663.865388467515!2d30.88893656523436!3d29.922386600000006!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sar!2seg!4v1747238723221!5m2!1sar!2seg"
                     width="596"
                     height="716"
                     style={{ border: 0, marginTop: '10px' }}
