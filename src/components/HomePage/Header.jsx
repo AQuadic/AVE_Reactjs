c068617f6a276e071d0632172b2014344f95ab9c
ahmed <ahmedabozaid.dev@gmail.com>
2025-06-02 14:34:59 +0300

    changes hero size

--- src/components/HomePage/Header.jsx
+++ src/components/HomePage/Header.jsx
@@ -2,7 +2,7 @@ import { useTranslation } from "react-i18next";
 // import heroImage from "../../assets/images/home/heroImage.png";
 import { Link } from "react-router";
 
-import newHero from '../../assets/images/home/newHero.png'
+import newHero from "../../assets/images/home/newHero.png";
 
 const Header = () => {
   const { t } = useTranslation("home");
@@ -18,20 +18,26 @@ const Header = () => {
           <p className="text-[#505050] lg:text-2xl text-sm md:font-normal font-bold md:mt-6 mt-2 lg:!leading-[40px] text-center max-w-[1000px]">
             {t("headTitle")}
           </p>
-          <div className=" md:mt-8 mt-6 flex md:gap-10 gap-6">
-            <Link to='/contactUs'
+          <div className="flex gap-6 mt-6 md:mt-8 md:gap-10">
+            <Link
+              to="/contactUs"
               className=" flex items-center justify-center lg:w-[215px] w-[160px] md:h-14 h-12 bg-[#D82022] text-white font-bold text-lg rounded-sm"
             >
               {t("prepareCar")}
             </Link>
-            <Link to='/contactUs'
+            <Link
+              to="/contactUs"
               className="flex items-center justify-center lg:w-[215px] w-[160px] md:h-14 h-12 border border-[#D82022] text-[#D82022] font-bold text-lg rounded-sm"
             >
               {t("buyCar")}
             </Link>
           </div>
         </section>
-        <img src={newHero} alt="hero" />
+        <img
+          className="max-w-[300px] sm:max-w-[900px] mx-auto"
+          src={newHero}
+          alt="hero"
+        />
       </div>
     </header>
   );
