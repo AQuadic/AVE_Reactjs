5b0eedc1e3a89ada0538258d3d0cdf12cac2a636
ahmed <ahmedabozaid.dev@gmail.com>
2025-06-02 13:52:51 +0300

    fixes certificates in who we are

--- src/components/general/OurCertificates.jsx
+++ src/components/general/OurCertificates.jsx
@@ -12,7 +12,7 @@ const OurCertificates = () => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 2.7,
+    slidesToShow: 2.86,
     slidesToScroll: 1,
     arrows: false,
     autoplay: true,
